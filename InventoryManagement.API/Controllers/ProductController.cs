e5a8d46cc0686fb47cd33624161e3b1ee4b80aec
Kamil Brucki <brucek12@interia.pl>
2026-06-01 15:51:33 +0200

    Add post product method to controller

---
+++ InventoryManagement.API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using InventoryManagement.Application.Products.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController(IMediator mediator) : ControllerBase
+    {
+        //[HttpGet]
+        //public async Task<IActionResult> GetRestaurants()
+        //{
+        //    var result = await mediator.Send(new GetRestaurantsQuery());
+        //    return Ok(result.Select(i => i.MapToRestaurantSimpleResponse()));
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductCommand command)
+        {
+            var result = await mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
